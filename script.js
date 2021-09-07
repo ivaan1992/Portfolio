2f0436c47821aa244c5834d631718ea8e952636c
ivaan1992 <ivaan.lg.92@gmail.com>
2021-09-07 11:45:58 -0500

    Fix all linters issues

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-var menu = document.querySelector(".menu")
-var ham = document.querySelector(".ham")
-var xIcon = document.querySelector(".xIcon")
-var menuIcon = document.querySelector(".menuIcon")
-
+const menu = document.querySelector('.menu');
+const ham = document.querySelector('.ham');
+const xIcon = document.querySelector('.xIcon');
+const menuIcon = document.querySelector('.menuIcon');
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    xIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    xIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
   } else {
-    menu.classList.add("showMenu");
-    xIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    menu.classList.add('showMenu');
+    xIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
 }
-ham.addEventListener("click", toggleMenu)
+ham.addEventListener('click', toggleMenu);
 
-
-var menuLinks = document.querySelectorAll(".menuLink")
+const menuLinks = document.querySelectorAll('.menuLink');
 
 menuLinks.forEach(
-  function (menuLink) {
-    menuLink.addEventListener("click", toggleMenu)
-  }
+  function menuLink(menuLink) {
+    menuLink.addEventListener('click', toggleMenu);
+  },
 );
